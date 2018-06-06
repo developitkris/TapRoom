4dadcc2aeda648f667644492e40cb0eec137e766
Jimmy McNamara and Kristi Hwang <kristi@>
2018-06-06 11:33:21 -0700

    initialize project, create class Beer with constructors, add list of beers, create buttons, add methods to buttons, finish two-way binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Beer } from './models/beer.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  name: string = 'Beer';
+
+  beers: Beer[]=[
+    new Beer('IceHouse', 'Miller', 4.62, 1.2),
+    new Beer('Sea Witch', 'Sea Pine', 5.3, 2.9),
+    new Beer('Fremont IPA', 'Fremont', 7.1, 2.7)
+  ]
+  lowKegs: Beer[] = [];
+  selectedBeer: null;
+
+  pourPint(clickedBeer){
+    if (0 >= clickedBeer.keg) {
+      alert("THIS KEG IS TAPPED")
+    } else if (clickedBeer < 10) {
+      clickedBeer.keg --;
+      // lowKegs.push(clickedBeer);
+    } else {
+      clickedBeer.keg --;
+    }
+  }
+  updatePour(pourAmt){
+     if (pourAmt.keg <= 10){
+      console.log("your keg is low on beer");
+      return "bg-warning";
+    }
+  }
+
+  editBeer(){
+    // this.beers = [];
+    alert("edit" + this.beers[0]);
+  }
 }
---
+++ src/app/models/beer.model.ts
@@ -0,0 +1,12 @@
+export class Beer {
+
+  constructor(
+    public name: string,
+    public brewery: string,
+    public abv: number,
+    public price: number,
+    public keg: number = 12)
+    {
+
+  }
+}
